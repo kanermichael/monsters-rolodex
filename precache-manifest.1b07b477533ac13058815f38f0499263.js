e92807d6b0f4985909e7f07b89b26b6d971c0603
Michael Kaner <michael@Michaels-MacBook-Pro-2.local>
2020-03-14 19:16:13 +0100

    Updates

---
+++ precache-manifest.1b07b477533ac13058815f38f0499263.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5aa9ce727fb9bdb1563d368eb34240",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f22ba717c49013d77f8f",
+    "url": "/monsters-rolodex/static/css/main.39c78ae9.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22ba717c49013d77f8f",
+    "url": "/monsters-rolodex/static/js/main.00ac4c33.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
